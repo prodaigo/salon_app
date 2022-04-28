553c5e3435fe418aec8ae99259f52c78d0cc34d5
prodaigo <soccer.0125.dtp@gmail.com>
2022-04-28 15:06:26 +0900

    カラムとバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と半角数字の両方を含めて設定してください'
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'カナ(全角)文字を使用してください' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end
