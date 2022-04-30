9d7880d914b65f6a5c4b78147bcaad7a3d668744
prodaigo <soccer.0125.dtp@gmail.com>
2022-04-30 16:18:37 +0900

    ユーザー編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,31 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: :show
+  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
+  end
+
+  def edit
+    unless @user == current_user
+      render :show
+    end
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.fetch(:user, {}).permit(:nickname, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   }
   root to: "mains#index"
   resources :mains, only: :index
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
 end
 
