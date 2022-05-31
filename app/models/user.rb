60546d7dc8603b0ace286de4a6f5fd2338efbf64
prodaigo <soccer.0125.dtp@gmail.com>
2022-05-31 22:43:14 +0900

    【update】バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   validates :nickname, presence: true
+  validates :birthday, presence: true
 
   has_many :sns_credentials
   has_many :staffs
